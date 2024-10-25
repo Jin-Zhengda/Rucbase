6bd478018ee3c60d0253e88a6b3e13e440798c83
Jin-Zhengda <jinzhengda7@gmail.com>
2024-10-25 22:01:15 +0800

    lab1: lry_replacer finished

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -27,8 +27,15 @@ bool LRUReplacer::victim(frame_id_t* frame_id) {
     // Todo:
     //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
     //  选择合适的frame指定为淘汰页面,赋值给*frame_id
-
-    return true;
+    if (LRUlist_.empty()) {
+        frame_id = nullptr;
+        return false;
+    } else {
+        *frame_id = LRUlist_.back();
+        LRUhash_.erase(*frame_id);
+        LRUlist_.pop_back();
+        return true;
+    }
 }
 
 /**
@@ -40,6 +47,10 @@ void LRUReplacer::pin(frame_id_t frame_id) {
     // Todo:
     // 固定指定id的frame
     // 在数据结构中移除该frame
+    if (LRUhash_.find(frame_id) != LRUhash_.end()) {
+        LRUlist_.erase(LRUhash_[frame_id]);
+        LRUhash_.erase(frame_id);
+    }
 }
 
 /**
@@ -50,6 +61,14 @@ void LRUReplacer::unpin(frame_id_t frame_id) {
     // Todo:
     //  支持并发锁
     //  选择一个frame取消固定
+    std::scoped_lock lock{latch_};
+
+    if (LRUhash_.find(frame_id) != LRUhash_.end()) {
+        return;
+    }
+
+    LRUlist_.push_front(frame_id);
+    LRUhash_[frame_id] = LRUlist_.begin();
 }
 
 /**
