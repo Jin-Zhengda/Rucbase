a9c1a7c362fe3d4e8ffd2821450600aa9a6ff38b
Jin-Zhengda <jinzhengda7@gmail.com>
2024-10-31 12:22:31 +0800

    lab1: rm_file_handle, rm_scan finished

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -18,7 +18,9 @@ See the Mulan PSL v2 for more details. */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // Todo:
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+    file_handle_ = file_handle;
+    rid_ = Rid{RM_FIRST_RECORD_PAGE, -1};
+    next();
 }
 
 /**
@@ -27,7 +29,17 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
 void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
-
+    for (; rid_.page_no < file_handle_->file_hdr_.num_pages; rid_.page_no++) {
+        RmPageHandle page_handle = file_handle_->fetch_page_handle(rid_.page_no);
+        rid_.slot_no =
+            Bitmap::next_bit(true, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page, rid_.slot_no);
+        if (rid_.slot_no == file_handle_->file_hdr_.num_records_per_page) {
+            rid_.slot_no = -1;
+        } else {
+            return;
+        }
+    }
+    rid_ = Rid{RM_NO_PAGE, -1};
 }
 
 /**
@@ -35,13 +47,12 @@ void RmScan::next() {
  */
 bool RmScan::is_end() const {
     // Todo: 修改返回值
-
-    return false;
+    return rid_.page_no == RM_NO_PAGE;
 }
 
 /**
  * @brief RmScan内部存放的rid
  */
-Rid RmScan::rid() const {
-    return rid_;
+Rid RmScan::rid() const { 
+    return rid_; 
 }
